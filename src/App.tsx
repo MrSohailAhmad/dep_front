0d39b077730c8df73d6279b9a8b41eb4d3fe5c4f
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-14 11:38:53 +0500

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex items-center justify-center w-full h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
