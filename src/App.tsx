1af427d39deed2d022d5ae1763ed096d98ec4f18
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-14 11:42:56 +0500

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="flex items-center justify-center w-full h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
         <span className="uppercase text-white font-bold text-xl">
-          this Muhammad Sohail site and its under development
+          this is Muhammad Sohail site and its under development
         </span>
       </div>
     </>
